6888d4677f3ab5c53f423d4d3b0d2cda1a34ca61
Alexdata <166223843+AlexData-Hawkhill@users.noreply.github.com>
2025-11-29 15:15:50 +0100

    Update clean-temp-files.sh
    Signed-off-by: Alexdata <166223843+AlexData-Hawkhill@users.noreply.github.com>

--- scripts/clean-temp-files.sh
+++ scripts/clean-temp-files.sh
@@ -26,19 +26,19 @@ echo "📦 Cleaning package cache (keeping last 3 versions)..."
 sudo paccache -r
 echo ""
 
-# 3. Clean pacman package cache (everything):
+# 3. Clean pacman package cache (everything temp that can be removed):
 echo "🔧 Cleaning PACMAN cache..."
 sudo pacman -Sc --noconfirm
 echo ""
 
-# 4. Clean paru/AUR cache (cleans even more than pacman does):
+# 4. Clean paru/AUR cache (cleans even more temp than pacman does):
 echo "🔧 Cleaning AUR cache..."
 sudo paru -Sc --noconfirm
 echo ""
 
-# 5. Remove old journal logs (keep last 3 days):
+# 5. Remove old journal logs (keep last 2 days):
 echo "📜 Cleaning old logs..."
-sudo journalctl --vacuum-time=3d
+sudo journalctl --vacuum-time=2d
 echo ""
 
 # 6. Show disk space saved:
@@ -46,7 +46,14 @@ echo "💾 Disk usage after cleaning:"
 df -h / | tail -1
 echo ""
 
-# 7. Python if user wants to delete that (present user with Y/n prompt for python - since rebuilding wheels take time!):
+# 7. Clean pacman/paru/AUR package cache (complete delete! also delete stored packages!):
+echo "🔧 COMING SOON -- Cleaning PACMAN PARU AUR cache for all downloaded and stored packages..."
+#read -p "Do you want to delete all cahced downloaded installer packages? Y/n?" USER_ANSWER
+#if $USER_ANSWER=lowercase("y" or "yes") + pacman=true then --> sudo pacman -Scc --noconfirm
+#if $USER_ANSWER=lowercase("y" or "yes") + paru=true then --> sudo paru -Scc --noconfirm
+echo ""
+
+# 8. Python if user wants to delete that (present user with Y/n prompt for python - since rebuilding wheels take time!):
 # Add logic for presenting user with a choice (e.g. read -p "select Y/n" + logic to handle that)
 # PS: NB: NON SUDO COMMANDS (WE WANT TO DELETE USERS PIP CACHE - NOT EMPTY ROOT CACHE)
 #exit
@@ -56,6 +63,11 @@ echo ""
 #pip cache purge      # Empty cache!
 #pip cache remove *   # Completely empty cache!
 #rm -rf ~/.cache/pip  # Empty and delete cache folder!
+
+# 9. Show disk space saved:
+echo "💾 Disk usage after cleaning:"
+df -h / | tail -1
+echo ""
 
 # Done !!
 echo "✅ Cleanup complete!"
